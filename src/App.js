06e6831c67528e0e865b18b479673a3633dcb981
Md. Mahmudul Hasan <mdhasan71224@gmail.com>
2022-03-26 15:23:32 +0600

    add description

--- src/App.js
+++ src/App.js
@@ -10,7 +10,17 @@ function App() {
       <Shop></Shop>
 
       <div className='Defination'>
-          <h2>How To Work React</h2>
+          <h2>How To Work React:</h2>
+          <p>Ans: React works in declarative code. To show what we mean by declarative code, we want you to imagine the following code expressed as an app. What you picture could look like the screen below, with a navbar, a header, a filter, and a list. That's because each line of code declares what each element of the app is.</p>
+
+          <h2>difference between state and props:</h2>
+          <p><strong>State: </strong>
+            The state is an updatable structure that is used to contain data or information about the component and can change over time. The change in state can happen as a response to user action or system event. It is the heart of the react component which determines the behavior of the component and how it will render. A state must be kept as simple as possible. It represents the component's local state or information. It can only be accessed or modified inside the component or by the component directly.</p>
+            <p><strong>Props: </strong>
+            Props are read-only components. It is an object which stores the value of attributes of a tag and work similar to the HTML attributes. It allows passing data from one component to other components. It is similar to function arguments and can be passed to the component the same way as arguments passed in a function. Props are immutable so we cannot modify the props from inside the component.</p>
+
+            <h2>How to work useState:</h2>
+            <p>useState is a Hook (function) that allows you to have state variables in functional components. You pass the initial state to this function and it returns a variable with the current state value (not necessarily the initial state) and another function to update this value</p>
       </div>
     </div>
   );
